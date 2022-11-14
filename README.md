abd33b003c6d567b3a7ffd53e94ff37883232415
wangkz <wangkz@infore.com>
2022-11-14 13:50:55 +0800

    build: component upgrade to 1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # VersionCatalog
+AGP 7.0.0以上依赖库统一版本号管理。
+
+[![](https://jitpack.io/v/FPhoenixCorneaE/VersionCatalog.svg)](https://jitpack.io/#FPhoenixCorneaE/VersionCatalog)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,12 +24,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
+    namespace = "com.fphoenixcorneae.version.catalog"
 }
 
 dependencies {
